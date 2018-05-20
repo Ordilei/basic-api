06374110a2dad9303b591c50930249a3d15897d7
Ordilei Souza <ordileisouza@CX-Ordilei.local>
2018-05-20 17:16:03 -0300

    Added script terraform for up ec2 to aws

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+  default = "AWS_ACCESS_KEY_ID"
+}
+
+variable "aws_secret_key" {
+  default = "AWS_SECRET_ACCESS_KEY"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
